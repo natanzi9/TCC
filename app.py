aa9bada179a4019e43b1e21fd76eb096befe1033
natan <natanreis564@gmail.com>
2026-05-18 07:38:08 -0300

    login diferente

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+def banco():
+    conecxao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="sua_senha",
+        database="nome_do_banco"
+    )
+    return conecxao
 
 @app.route('/')
 def login():
